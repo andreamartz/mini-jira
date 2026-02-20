c2ae68442910dea2da3aedf4e053ae1d42555e7c
Andrea Martz <14165788+andreamartz@users.noreply.github.com>
2026-02-20 15:43:46 -0600

    Render mock Kanban board structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,42 @@
 import "./App.css";
+import { mockData } from "./mock/mockBoard";
 
 export default function App() {
+  const { board, columnsById, cardsById } = mockData;
+  const { columnIds } = board;
+
   return (
-    <div style={{ padding: 24, fontFamily: "system-ui, sans-serif" }}>
-      <h1>Mini Jira</h1>
+    <>
+      <h1>{board.title}</h1>
       <p>Scaffold is working ✅</p>
-    </div>
+      <div>
+        {columnIds.map((colId) => {
+          const column = columnsById[colId];
+          if (!column) return null;
+
+          const { cardIds } = column;
+          const { title: colTitle } = column;
+          return (
+            <section key={colId}>
+              <h2>Column title: {colTitle}</h2>
+              <ul>
+                {cardIds.map((cardId) => {
+                  const card = cardsById[cardId];
+                  if (!card) return null;
+
+                  const { title: cardTitle } = card;
+                  return (
+                    <li key={cardId}>
+                      Card title: { cardTitle }
+                    </li>
+                  );
+                })
+              }
+              </ul>
+            </section>
+          );
+        })}
+      </div>
+    </>
   );
 }
